501aae668bf57ddc426ef8dddddd68bcb88e16dc
ronakmehtav <ronakmehtav@gmail.com>
2023-07-02 13:35:43 +0530

    Able to read json data from body.

--- background.js
+++ background.js
@@ -1,5 +1,7 @@
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  if (changeInfo.status === 'complete' && tab.url.startsWith("https")) {
-    chrome.tabs.executeScript(tabId, { code: "alert('Hello World');" });
+  if (changeInfo.status === 'complete' && tab.url.match("https://jsonplaceholder.typicode.com/todos/1")) {
+    chrome.tabs.executeScript(tabId, { code: `
+            alert(JSON.parse(document.body.textContent).title)
+        ` });
   }
 });
